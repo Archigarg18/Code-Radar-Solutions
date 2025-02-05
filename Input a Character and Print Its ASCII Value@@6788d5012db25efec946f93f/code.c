edda66e6bd55d43d07850c1a8053a20a90caf914
Archigarg18 <archigarg9888@gmail.com>
2025-02-05 09:41:52 +0530

    Solution to Input a Character and Print Its ASCII Value

--- Input a Character and Print Its ASCII Value@@6788d5012db25efec946f93f/code.c
+++ Input a Character and Print Its ASCII Value@@6788d5012db25efec946f93f/code.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main() {
     char str[100];
-    
+    int a;
     printf("");
     scanf("%s",&str);
-    printf("ASCII Value: %s\n",str);
+    printf("ASCII Value: %d\n",a);
     return 0;
 }
