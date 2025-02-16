8374de547553b87e152fe7c6beecd33baf04a183
Archi <archigarg9888@gmail.com>
2025-02-16 11:17:14 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-
 int main() {
     double a,b;
     char operator;
     printf("");
     scanf("%lf", &a);
     printf("");
-    scanf(" %c", &operator); 
-    printf("");
     scanf("%lf",&b);
+    printf("");
+    scanf(" %c", &operator); 
     switch (operator) {
         case '+':
             printf("%.2lf + %.2lf = %.2lf\n",a,b, a + b);
